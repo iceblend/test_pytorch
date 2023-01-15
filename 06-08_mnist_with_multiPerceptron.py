0b28740f7b849751420cb08809fdafd4c6f166d8
iceblend <mildroast@naver.com>
2023-01-15 20:24:42 +0900

    modified

--- 06-08_mnist_with_multiPerceptron.py
+++ 06-08_mnist_with_multiPerceptron.py
@@ -1,5 +1,7 @@
 # https://wikidocs.net/61073
 import torch
+from torch import nn
+from torch import optim
 from torch.utils.data import TensorDataset, DataLoader
 from sklearn.model_selection import train_test_split
 
@@ -9,20 +11,39 @@ from sklearn.datasets import fetch_openml
 
 mnist = fetch_openml('mnist_784', version=1, cache=True)
 
-X = mnist.data / 255.0
+X = mnist.data / 255
 y = mnist.target
 
-print(X[0])
-print(y[0])
+print(X.shape)
+print(y.shape)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/7, random_state=0)
 
+X_train = torch.Tensor(X_train)
+X_test = torch.Tensor(X_test)
+y_train = torch.LongTensor(y_train)
+y_test = torch.LongTensor(y_test)
 
+ds_train = TensorDataset(X_train, y_train)
+ds_test = TensorDataset(X_test, y_test)
 
+loader_train = DataLoader(ds_train, batch_size=64, shuffle=True)
+loader_test = DataLoader(ds_test, batch_size=64, shufffle=False)
 
+model = nn.Sequential()
+model.add_module('fc1', nn.Linear(28 * 28 * 1, 100))
+model.add_module('relu1', nn.ReLU())
+model.add_module('fc2', nn.Linear(100, 100))
+model.add_module('relu2', nn.ReLU())
+model.add_module('fc3', nn.Linear(100, 100))
 
+print(model)
 
+# 오차함수 선택
+loss_fn = nn.CrossEntropyLoss()
 
-
+# 가중치를 학습하기 위한 최적화 기법 선태
+optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 
 
